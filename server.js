2e41ae5359fb857d232ac1c4959b1710751d46d0
shukirtha <32018717+shukirtha@users.noreply.github.com>
2017-09-16 21:48:35 +0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-two', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'article-two'));  
+res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));  
 });
 
 app.get('/ui/style.css', function (req, res) {
